4e0d526d765eae04a1ddada173710f6ff7c765fe
dvo-midwayusa <dvo@midwayusa.com>
2017-03-15 00:25:28 -0500

    3 working demos

--- src/main.js
+++ src/main.js
@@ -2,14 +2,41 @@
 
 let count = 0
 
-function addToDo() {
+function demo0() {
+
+    let todo = document.getElementById('input')
+
+    document.write(todo.value)
+}
+
+function demo1() {
 
     let todo = document.getElementById('input'),
         node = document.createElement('li')
 
-    node.innerHTML = `<input type="checkbox" /><span>${todo.value}</span>`
+    node.innerHTML = '<input type="checkbox" /><span>' + todo.value + '</span>'
 
     document.getElementById('column' + (count % 2 + 1)).appendChild(node)
+
+    todo.value = ''
+    count++
+}
+
+function demo2() {
+
+    let todo = document.getElementById('input'),
+        checkbox = document.createElement('input'),
+        listItem = document.createElement('li'),
+        span = document.createElement('span'),
+        textNode = document.createTextNode(todo.value)
+
+    checkbox.type = 'checkbox'
+    span.appendChild(textNode)
+
+    listItem.appendChild(checkbox)
+    listItem.appendChild(span)
+
+    document.getElementById('column' + (count % 2 + 1)).appendChild(listItem)
 
     todo.value = ''
     count++
